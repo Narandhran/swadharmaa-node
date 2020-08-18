3e50cca2f4024d88cbd75bf39ff30c3816624755
narandhran <narandhran@gmail.com>
2020-08-18 08:25:27 +0530

    db auth added

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const { Config } = require('aws-sdk');
 // establish connection to mongodb
 var database = {
     connectivity: (config) => {
         mongoose.set('debug', true);
         mongoose.Promise = global.Promise;
-        mongoose.connect(`${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`, { useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true });
+        mongoose.connect(`mongodb://${config.DB_USER}:${config.DB_PASS}@${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`,
+            { useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true });
         const db = mongoose.connection;
         return db;
     }
